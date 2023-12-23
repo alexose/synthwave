2e33f235771e997215b1f2da86d4592918998d4a
Alexander Ose <alexander@parsely.com>
2023-12-23 11:31:32 -0800

    Update software for routines

--- client/src/components/MainControls.vue
+++ client/src/components/MainControls.vue
@@ -2,16 +2,48 @@
     <div class="hello">
         <h1>Synthwave</h1>
     </div>
-    <div class="device-list">
-        <div
-            v-for="(device, idx) in devices"
-            :key="'device-' + idx"
-            :class="['device', status[idx] ? 'active' : null]"
-            @click="activate(device)"
-        >
-            {{ device.name }}
-        </div>
+
+    <span>Current Status: {{ currentStatus }}</span>
+
+    <div class="single-device">
+        <div class="device">Begin Cycle</div>
     </div>
+
+    <hr class="divider" />
+
+    <details class="devices">
+        <summary>
+            <span class="title">Routines</span>
+        </summary>
+        <div class="device-list">
+            <div
+                v-for="(routine, idx) in routines"
+                :key="'routine-' + idx"
+                :class="['device', status[idx] ? 'active' : null]"
+                @click="activate(routine)"
+            >
+                {{ routine.name }}
+            </div>
+        </div>
+    </details>
+
+    <hr class="divider" />
+
+    <details class="devices">
+        <summary>
+            <span class="title">Devices</span>
+        </summary>
+        <div class="device-list">
+            <div
+                v-for="(device, idx) in devices"
+                :key="'device-' + idx"
+                :class="['device', status[idx] ? 'active' : null]"
+                @click="activate(device)"
+            >
+                {{ device.name }}
+            </div>
+        </div>
+    </details>
 </template>
 
 <script>
@@ -73,7 +105,42 @@
         data() {
             return {
                 intervalId: null,
+                currentStatus: "Idle",
                 status: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                routines: [
+                    {
+                        name: "Cycle Left",
+                        key: "CYCLE1",
+                    },
+                    {
+                        name: "Cycle Right",
+                        key: "CYCLE2",
+                    },
+                    {
+                        name: "Fill Left Container",
+                        key: "FILL1",
+                    },
+                    {
+                        name: "Fill Right Container",
+                        key: "FILL2",
+                    },
+                    {
+                        name: "Transfer Left to Right",
+                        key: "TRANSFER1TO2",
+                    },
+                    {
+                        name: "Transfer Right to Left",
+                        key: "TRANSFER1TO2",
+                    },
+                    {
+                        name: "Drain Left",
+                        key: "DRAIN1",
+                    },
+                    {
+                        name: "Drain Right",
+                        key: "DRAIN2",
+                    },
+                ],
                 devices: [
                     {
                         name: "Pump 1",
@@ -129,14 +196,20 @@
     .device-list {
         display: grid;
         max-width: 500px;
-        margin: 0 auto;
+        margin: 20px auto;
         grid-template-columns: repeat(2, 1fr); /* 2 columns that take up an equal fraction of the space */
         grid-template-rows: repeat(4, auto); /* 4 rows that adjust to the content height */
         gap: 16px; /* Space between grid items, adjust as necessary */
     }
 
+    .single-device {
+        width: 300px;
+        margin: 40px auto 0;
+    }
+
     .device {
-        border: 1px solid #eee;
+        border: 1px solid #ddd;
+        border-radius: 5px;
         margin: 10px;
         padding: 20px;
         cursor: pointer;
@@ -144,5 +217,21 @@
 
     .device.active {
         background: #eee;
+    }
+
+    .divider {
+        margin: 40px auto;
+        max-width: 500px;
+        border-top: 1px solid #eee;
+    }
+
+    .devices {
+        margin-top: 40px;
+    }
+
+    .title {
+        font-size: 1.2em;
+        font-weight: bold;
+        cursor: pointer;
     }
 </style>
